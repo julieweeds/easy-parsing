cd379d3353328c770b8d8ede4061e63c120465f3
julieweeds <julie.weeds@gmail.com>
2015-10-14 14:48:21 +0100

    clean Stanford conll format

--- src/main/java/uk/ac/susx/tag/easyparsing/ParserApp.java
+++ src/main/java/uk/ac/susx/tag/easyparsing/ParserApp.java
@@ -1,7 +1,7 @@
 package uk.ac.susx.tag.easyparsing;
 import uk.ac.susx.tag.dependencyparser.Parser;
-import java.io.File;
-import java.io.IOException;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -21,6 +21,7 @@ public class ParserApp extends EasyFileVisitor{
     private final String outdir;
     private String inputformat;
     private String outputformat;
+    private int inputlength;
 
     ParserApp(String indir) throws IOException {
         myparser=new Parser("full_wsj_penn_pos_stanford_dep");
@@ -28,12 +29,14 @@ public class ParserApp extends EasyFileVisitor{
         this.outputformat="id, form, lemma, pos, ner, head, deprel";
         if (this.indir.endsWith("sfd_parsed")){
             this.outdir=indir.replaceAll("sfd_parsed","parsed");
-            this.inputformat=this.outputformat;
+            this.inputformat="id, form, lemma, pos, head, deprel";
+            this.outputformat=this.inputformat;
+
         } else {
             this.outdir = indir.replaceAll("tagged", "parsed");
             this.inputformat="id, form, lemma,pos,ner";
         }
-
+        this.inputlength=this.inputformat.split(",").length;
         if (Files.notExists(Paths.get(outdir))){
             Files.createDirectories(Paths.get(outdir));
         }
@@ -68,14 +71,73 @@ public class ParserApp extends EasyFileVisitor{
 
             File outfile = new File(outfilename);
 
+
+            if (this.indir.endsWith("sfd_parsed")){
+                File tempfile= new File("tempfile");
+                try {
+                    this.clean(infile, tempfile);
+                } catch (IOException e){
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                infile=tempfile;
+
+            }
             try {
                 myparser.batchParseFile(infile, outfile, this.inputformat, this.outputformat, "");
             } catch (IOException e) {
                 e.printStackTrace();
+                System.exit(1);
             }
         } else {
             System.out.println("Ignoring files without conll extension "+filename);
         }
+    }
+
+    public void clean(File infile, File outfile) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(infile),"UTF-8"));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outfile),"UTF-8"));
+
+        String line;
+
+        while((line=reader.readLine())!=null){
+            writer.write(cleanLine(line.trim()));
+            writer.newLine();
+        }
+
+
+
+        reader.close();
+        writer.close();
+
+    }
+
+    private String cleanLine(String line){
+        String[] fields = line.split("\t");
+        if (fields.length<2) {
+           return "";
+        }
+        else {
+            if (fields.length == this.inputlength) {
+                try {
+                    Integer.parseInt(fields[0]);
+                    Integer.parseInt(fields[4]);
+                } catch (Exception e) {
+                    System.err.println("Error line " + line);
+                    e.printStackTrace();
+                }
+                return line;
+            } else {
+                int padding = this.inputlength - fields.length;
+                while (padding > 0) {
+                    line += "\t" + "-1";
+                    padding--;
+                }
+                //System.err.println("Cleaned: "+line);
+                return line;
+            }
+        }
+
     }
 
     public static void main(String[] args){
